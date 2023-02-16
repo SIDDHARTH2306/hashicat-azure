440b671e8cc1f6e30b6884a366d8f86c4c9273b3
Siddharth2306 <siddharthanikar2306@gmail.com>
2023-02-16 20:10:00 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,8 +2,5 @@ module "network" {
   source  = "app.terraform.io/hashicat-azure-sid/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  variable "resource_group_name" {
-  description = "The name of resource group."
-  type        = string
-}
+  resource_group_name = "${var.prefix}-workshop"
 }
